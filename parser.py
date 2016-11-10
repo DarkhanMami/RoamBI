3869beba8bf3fe521b0809a50b2ac49114ceb71b
Darkhan Akzholov <dakzholov@gmail.com>
2016-11-11 04:53:31 +0600

    smart development

---
+++ parser.py
@@ -0,0 +1,175 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import poplib
+import string, random
+import StringIO, rfc822
+import os
+import email
+import xlrd
+from collections import OrderedDict
+import simplejson as json
+from sets import Set
+from datetime import datetime
+
+
+
+SERVER = "pop.gmail.com"
+USER  = "darkhan.mami@gmail.com"
+PASSWORD = "!WANrltw92"
+DIR = "/Users/darkhan/Dropbox/mobile/RoamBI/"
+
+
+def getAttachment(emails):
+    for mail in emails:
+        for part in mail.walk():
+            print(part.get_content_type())
+
+            if part.get_content_maintype() == 'multipart':
+                continue
+
+            if part.get('Content-Disposition') is None:
+                print("no content dispo")
+                continue
+
+            filename = part.get_filename()
+            if not(filename): filename = "Pulse.xlsx"
+            print(filename)
+
+            fp = open(os.path.join(DIR, filename), 'wb')
+            fp.write(part.get_payload(decode=1))
+            fp.close()
+
+
+
+def fileParse():
+    wb = xlrd.open_workbook(DIR + 'Pulse.xlsx')
+    sh = wb.sheet_by_index(0)
+
+    fullData = dict()
+    smartData = dict()
+    places = []
+    colNames = ['Объект','Месяц','Дата','Замер','техрежим','Резервуар','Сдача нефти','техрежим нефть','добыча нефти','Рас. т/р','зам. доб. с нак','Доб. ж. по рез. с нак.','Сд. н с нак.','доб. н. по рез. с нак']
+    plSet = Set([])
+
+
+    col_dict = OrderedDict()
+    col_dict['colNames'] = colNames
+
+    j = json.dumps(col_dict)
+    with open('colNames.json', 'w') as f:
+        f.write(j)
+
+    monthes = []
+    for rownum in range(1, sh.nrows):
+        row_values = sh.row_values(rownum)
+        temp = []
+
+        temp.append(row_values[0])
+        temp.append(row_values[1])
+        if row_values[1] not in monthes:
+            monthes.append(row_values[1])
+            fullData[row_values[1]] = list()
+        a1_tuple = xlrd.xldate_as_tuple(row_values[2], wb.datemode)
+        a1_datetime = datetime(*a1_tuple)
+        temp.append(a1_datetime.strftime("%d/%m/%Y"))
+
+        temp.append(row_values[3])
+        temp.append(row_values[4])
+        temp.append(row_values[5])
+        temp.append(row_values[6])
+        temp.append(row_values[7])
+        temp.append(row_values[8])
+        temp.append(row_values[9])
+        temp.append(row_values[10])
+        temp.append(row_values[11])
+        temp.append(row_values[12])
+        temp.append(row_values[13])
+        if (a1_datetime.strftime("%d/%m/%Y") == "31/01/2016" and len(monthes) < 2 and len(row_values[0]) == 17):
+            print row_values[1] + row_values[0] + a1_datetime.strftime("%d/%m/%Y")
+        fullData[row_values[1]].append(temp)
+        if a1_datetime.strftime("%d/%m/%Y") == "01/01/2016" and len(row_values[0]) == 17:
+            print "------------"
+            print row_values[0]
+            print row_values[1]
+            print row_values[2]
+        smartData[row_values[1] + row_values[0] + a1_datetime.strftime("%d/%m/%Y")] = temp
+        if row_values[0] not in plSet:
+            plSet.add(row_values[0])
+            places.append(row_values[0])
+
+
+
+
+    pl_dict = OrderedDict()
+    pl_dict['places'] = places
+    j = json.dumps(pl_dict)
+    with open('places.json', 'w') as f:
+        f.write(j)
+
+
+
+
+    full_dict = OrderedDict()
+    full_dict['fullData'] = fullData
+    j = json.dumps(full_dict)
+
+    with open('fullData2.json', 'w') as f:
+        f.write(json.dumps(fullData))
+    with open('places2.json', 'w') as f:
+        f.write(json.dumps(places))
+    with open('colNames2.json', 'w') as f:
+        f.write(json.dumps(colNames))
+    with open('monthes2.json', 'w') as f:
+        f.write(json.dumps(monthes))
+
+    with open('smartData2.json', 'w') as f:
+        f.write(json.dumps(smartData))
+
+    with open('fullData.json', 'w') as f:
+        f.write(j)
+
+    with open('fullData.json', 'w') as f:
+        f.write(j)
+
+    for m in monthes:
+        print m
+
+
+
+
+
+
+
+
+
+
+
+server = poplib.POP3_SSL(SERVER)
+
+server.user(USER)
+server.pass_(PASSWORD)
+
+
+resp, items, octets = server.list()
+
+
+numMessages = len(items)
+
+for i in xrange(numMessages - 1, 0 , -1):
+    id, size = string.split(items[i])
+    resp, text, octets = server.retr(id)
+    raw_message = text
+
+    text = string.join(text, "\n")
+    file = StringIO.StringIO(text)
+    message = rfc822.Message(file)
+
+    if message['Subject'] == 'roambi':
+        emails = [email.message_from_string('\n'.join(raw_message))]
+        getAttachment(emails)
+        break
+
+fileParse()
+
+print 'DONE'
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -44,191 +44,217 @@ angular.module('starter.controllers', ['chart.js'])
 })
 
 
-.controller("BarCtrl", function($scope, Data, $ionicLoading) {
+.controller("BarCtrl", function($scope, Data, $ionicLoading, $http, $timeout) {
+    $scope.show = false;
+    var promise = Data.init();
+    promise.then(function(greeting) {
+        run();
+
+    }, function(reason) {
+        console.log(reason);
+    });
+
+    var run = function() {
+        $scope.colNames = Data.getColNames();
+        console.log('getting places');
+        $scope.places = Data.getPlaces(1, $scope.colNames[3], $scope.colNames[5]);
+        $scope.date = "1/01/2016";
+
+        // Test default name & columns
+        $scope.query = {
+            name: $scope.places[0].name,
+            colName1: $scope.colNames[3],
+            colName2: $scope.colNames[5]
+        }
+
+        $scope.b1 = Data.getSingleData($scope.query.name, 1, "Замер", "Резервуар");
+        $scope.b2 = Data.getSingleData($scope.query.name, 1, "Сдача нефти", "добыча нефти");
+        $scope.b3 = Data.getSingleData($scope.query.name, 1, "Рас. т/р", "техрежим");
+        $scope.b4 = Data.getSingleData($scope.query.name, 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
+
+        $scope.height = screen.height / 35;
+
+        $scope.onClick = function (points, evt) {
+          $scope.places = Data.getPlaces(points[1]._index + 1, $scope.query.colName1, $scope.query.colName2);
+
+          $scope.date = "" + (points[1]._index + 1) + "/01/2016";
+          $scope.b1 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Замер", "Резервуар");
+          $scope.b2 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Сдача нефти", "добыча нефти");
+          $scope.b3 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Рас. т/р", "техрежим");
+          $scope.b4 = Data.getSingleData($scope.query.name, points[1]._index + 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
 
 
-    $scope.colNames = Data.getColNames();
-    $scope.places = Data.getPlaces(1, $scope.colNames[3], $scope.colNames[5]);
-    $scope.date = "1/01/2016";
-
-    // Test default name & columns
-    $scope.query = {
-        name: $scope.places[0].name,
-        colName1: $scope.colNames[3],
-        colName2: $scope.colNames[5]
-    }
-
-    $scope.b1 = Data.getSingleData($scope.query.name, 1, "Замер", "Резервуар");
-    $scope.b2 = Data.getSingleData($scope.query.name, 1, "Сдача нефти", "добыча нефти");
-    $scope.b3 = Data.getSingleData($scope.query.name, 1, "Рас. т/р", "техрежим");
-    $scope.b4 = Data.getSingleData($scope.query.name, 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
-
-    $scope.height = screen.height / 35;
-
-    $scope.onClick = function (points, evt) {
-      $scope.places = Data.getPlaces(points[1]._index + 1, $scope.query.colName1, $scope.query.colName2);
-
-      $scope.date = "" + (points[1]._index + 1) + "/01/2016";
-      $scope.b1 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Замер", "Резервуар");
-      $scope.b2 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Сдача нефти", "добыча нефти");
-      $scope.b3 = Data.getSingleData($scope.query.name, points[1]._index + 1, "Рас. т/р", "техрежим");
-      $scope.b4 = Data.getSingleData($scope.query.name, points[1]._index + 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
+          $scope.$apply();
+        };
 
 
-      $scope.$apply();
-    };
+        $scope.updateData = function(name, colName1, colName2) {
+    /*
+            $ionicLoading.show({
+              duration: 500,
+              template: '<ion-spinner icon="ios"></ion-spinner>'
+            });
+    */
 
+            $scope.result = Data.getNumbers(name, colName1, colName2);
+            $scope.query.colName1 = colName1;
+            $scope.query.colName2 = colName2;
 
-    $scope.updateData = function(name, colName1, colName2) {
-/*
-        $ionicLoading.show({
-          duration: 500,
-          template: '<ion-spinner icon="ios"></ion-spinner>'
-        });
-*/
+            $scope.data = [
+              $scope.result.r1,
+              $scope.result.r2
+            ];
 
-        $scope.result = Data.getNumbers(name, colName1, colName2);
-        $scope.query.colName1 = colName1;
-        $scope.query.colName2 = colName2;
+            //$scope.series = [colName1, colName2];
 
-        $scope.data = [
-          $scope.result.r1,
-          $scope.result.r2
-        ];
-
-        //$scope.series = [colName1, colName2];
-
-        $scope.places = Data.getPlaces(1, colName1, colName2);
-        console.log('looping');
-        angular.forEach($scope.places, function(value, key) {
-          if (value.name == name) {
-              $scope.selectedItem = {
-                  "name": name,
-                  "v1": value.v1,
-                  "v2": value.v2,
+            $scope.places = Data.getPlaces(1, colName1, colName2);
+            console.log('looping');
+            angular.forEach($scope.places, function(value, key) {
+              if (value.name == name) {
+                  $scope.selectedItem = {
+                      "name": name,
+                      "v1": value.v1,
+                      "v2": value.v2,
+                  }
               }
-          }
-        }, null);
+            }, null);
 
 
-        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+            $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
 
-        $scope.options = {
-          scales: {
-            yAxes: [
-              {
-                id: 'y-axis-1',
-                type: 'linear',
-                display: true,
-                position: 'left'
+            $scope.options = {
+              scales: {
+                yAxes: [
+                  {
+                    id: 'y-axis-1',
+                    type: 'linear',
+                    display: true,
+                    position: 'left'
+                  },
+                  {
+                    id: 'y-axis-2',
+                    type: 'linear',
+                    display: true,
+                    position: 'right'
+                  }
+                ]
               },
-              {
-                id: 'y-axis-2',
-                type: 'linear',
-                display: true,
-                position: 'right'
-              }
-            ]
-          },
 
 
-          "legend": {
-          "display": false,
-          "position": "top"
+              "legend": {
+              "display": false,
+              "position": "top"
+            }
+
+           };
+
         }
 
-       };
 
-    }
+        $scope.updateData($scope.query.name, $scope.query.colName1, $scope.query.colName2);
+
+        $scope.raw_selected = function(name) {
+          $scope.query.name = name;
+          $scope.result = Data.getNumbers(name, $scope.query.colName1, $scope.query.colName2);
+          $scope.data = [
+              $scope.result.r1,
+              $scope.result.r2
+            ];
+            angular.forEach($scope.places, function(value, key) {
+              if (value.name == name) {
+                  $scope.selectedItem = {
+                      "name": name,
+                      "v1": value.v1,
+                      "v2": value.v2,
+                  }
+              }
+            }, null);
+          $scope.b1 = Data.getSingleData($scope.query.name, 1, "Замер", "Резервуар");
+          $scope.b2 = Data.getSingleData($scope.query.name, 1, "Сдача нефти", "добыча нефти");
+          $scope.b3 = Data.getSingleData($scope.query.name, 1, "Рас. т/р", "техрежим");
+          $scope.b4 = Data.getSingleData($scope.query.name, 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
+        }
 
 
-    $scope.updateData($scope.query.name, $scope.query.colName1, $scope.query.colName2);
+      $scope.result = Data.getNumbers($scope.query.name, $scope.query.colName1, $scope.query.colName2);
 
-    $scope.raw_selected = function(name) {
-      $scope.query.name = name;
-      $scope.result = Data.getNumbers(name, $scope.query.colName1, $scope.query.colName2);
+
+
+      $scope.labels = ['1', '', '', '', '5', '', '','', '', '10', '', '', '', '', '15', '', '', '', '', '20', '','', '', '', '25', '', '', '', '', '30', ''];
+
+      $scope.series = [$scope.query.colName1, $scope.query.colName2];
+
       $scope.data = [
-          $scope.result.r1,
-          $scope.result.r2
-        ];
-        angular.forEach($scope.places, function(value, key) {
-          if (value.name == name) {
-              $scope.selectedItem = {
-                  "name": name,
-                  "v1": value.v1,
-                  "v2": value.v2,
-              }
-          }
-        }, null);
-      $scope.b1 = Data.getSingleData($scope.query.name, 1, "Замер", "Резервуар");
-      $scope.b2 = Data.getSingleData($scope.query.name, 1, "Сдача нефти", "добыча нефти");
-      $scope.b3 = Data.getSingleData($scope.query.name, 1, "Рас. т/р", "техрежим");
-      $scope.b4 = Data.getSingleData($scope.query.name, 1, "зам. доб. с нак", "Доб. ж. по рез. с нак.");
+        $scope.result.r1,
+        $scope.result.r2
+      ];
+
+
+      //$scope.colors = ['#72C02C', '#3498DB', '#717984', '#F1C40F'];
+
+    $scope.colors = [
+                        {
+                            backgroundColor: '#00cc00',
+                            borderColor: '#00cc66',
+                            hoverBackgroundColor: '#A2DED0',
+                            hoverBorderColor: '#A2DED0'
+                        },
+                        {
+                            backgroundColor: '#0066ff',
+                            borderColor: '#3366ff',
+                            hoverBackgroundColor: '#65C6BB',
+                            hoverBorderColor: '#65C6BB'
+                        },
+
+                    ];
+
+
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      $scope.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        },
+
+
+        "legend": {
+        "display": false,
+        "position": "top"
+      }
+
+      };
+
+
+      $scope.show = true;
+      $scope.monthes = Data.getMonthes();
+      $scope.currentMonth = Data.getMonthes()[0];
     }
 
-
-  $scope.result = Data.getNumbers($scope.query.name, $scope.query.colName1, $scope.query.colName2);
-
-
-
-  $scope.labels = ['1', '', '', '', '5', '', '','', '', '10', '', '', '', '', '15', '', '', '', '', '20', '','', '', '', '25', '', '', '', '', '30', ''];
-
-  $scope.series = [$scope.query.colName1, $scope.query.colName2];
-
-  $scope.data = [
-    $scope.result.r1,
-    $scope.result.r2
-  ];
+    $scope.updateMonth = function(newMonth) {
+        console.log(newMonth);
+        $scope.currentMonth = newMonth;
+        console.log('updating month');
+        console.log('new month');
+        $scope.currentMonth = Data.getMonthes()[2];
+        console.log($scope.currentMonth);
+        Data.setCurrentMonth($scope.currentMonth);
+        $scope.show = false;
+        run();
 
 
-  //$scope.colors = ['#72C02C', '#3498DB', '#717984', '#F1C40F'];
-
-$scope.colors = [
-                    {
-                        backgroundColor: '#00cc00',
-                        borderColor: '#00cc66',
-                        hoverBackgroundColor: '#A2DED0',
-                        hoverBorderColor: '#A2DED0'
-                    },
-                    {
-                        backgroundColor: '#0066ff',
-                        borderColor: '#3366ff',
-                        hoverBackgroundColor: '#65C6BB',
-                        hoverBorderColor: '#65C6BB'
-                    },
-
-                ];
-
-
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
-      ]
-    },
-
-
-    "legend": {
-    "display": false,
-    "position": "top"
-  }
-
-  };
-
-
-
+    }
 
 
 });
